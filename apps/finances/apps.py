748169bd82674d9366bdd379ecbd4c267a5fe674
PerezsA-ux <perezalarconsebastian@gmail.com>
2025-10-15 00:06:48 -0500

    Finances v2

--- apps/finances/apps.py
+++ apps/finances/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FinancesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'finances'
+    name = 'apps.finances'
---
+++ apps/finances/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Category, Expense, SavingsGoal
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name,', 'icon', 'color', 'user']
+        read_only_fields = ['user']
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+
+    class Meta:
+        model = Expense
+        fields = [
+            'id', 'user', 'date', 'category', 'category_name',
+            'amount', 'description', 'created_at'
+        ]
+        read_only_fields = ['user', 'created_at']
+
+class SavingsGoalSerializer(serializers.ModelSerializer):
+    progress_percentage = serializers.SerializerMethodField()
+
+    class Meta:
+        model = SavingsGoal
+        fields = [
+            'id', 'user', 'name', 'target_amount',
+            'current_amount', 'deadline', 'progress_percentage'
+        ]
+        read_only_fields = ['user']
+
+    def get_progress_percentage(self, obj):
+        if obj.target_amount > 0:
+            return float(obj.current_amount / obj.target_amount) * 100
+        return 0
